95d7f888c3e953746dfcbe11829d07a5c625c524
karta1571234 <karta1571234@yahoo.com.tw>
2021-06-16 04:15:55 +0800

    hw_0401

---
+++ 0401/App3.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <Header/>
+        <Content/>
+    </div>
+);
+const Header = () => (
+    <div>
+      <h1>Header</h1>
+    </div>
+);
+const Content = () => (
+    <div>
+      <h2>Content</h2>
+      <p>The content text!!!</p>
+      <p>This is arrowFunction</p>
+    </div>
+);
+
+export default App;
